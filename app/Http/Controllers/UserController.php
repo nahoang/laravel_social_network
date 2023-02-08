2521148d0fbace86f22e8d6e4544142d58716481
nahoang <nahoang9698@gmail.com>
2023-02-08 14:44:03 +0700

    Initial source

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+
+class UserController extends Controller
+{
+
+    public function showCorrectHomepage() {
+        if (auth()->check()) {
+            return view('homepage-feed');
+        } else {
+            return view('homepage');
+        }
+    }
+
+    public function logout() {
+        auth()->logout();
+        return redirect('/')->with('success', 'You are now logged out.');
+    }
+
+    public function login(Request $request) {
+        $incomingFields = $request->validate([
+            'loginusername' => 'required',
+            'loginpassword' => 'required',
+        ]);
+
+        if (auth()->attempt(['username' => $incomingFields['loginusername'], 'password' => $incomingFields['loginpassword']])) {
+            $request->session()->regenerate();
+            return redirect('/')->with('success', 'You have successfully logged in.');
+        } else {
+            return 'Sorry!!!';
+        }
+    }
+
+    public function register(Request $request) {
+        $incomingFields = $request->validate([
+            'username' => ['required', 'min:3', 'max: 20', Rule::unique('users', 'username')],
+            'email' => ['required', 'email', Rule::unique('users', 'email')],
+            'password' => ['required', 'min:8', 'confirmed'],
+        ]);
+
+        $incomingFields['password'] = bcrypt($incomingFields['password']);
+
+        User::create($incomingFields);
+        return 'Hello from register function';
+    }
+}
