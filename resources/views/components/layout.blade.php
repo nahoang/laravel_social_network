96bc9f5ec2d9d76eb6cfe374ac1870d5f660d6fd
nahoang <nahoang9698@gmail.com>
2023-02-08 17:13:43 +0700

    feat: Add Post policy for updating, deleting

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -23,13 +23,13 @@
           <span class="text-white mr-2 header-chat-icon" title="Chat" data-toggle="tooltip" data-placement="bottom"><i class="fas fa-comment"></i></span>
           <a href="/profile/{{auth()->user()->username}}" class="mr-2"><img title="My Profile" data-toggle="tooltip" data-placement="bottom" style="width: 32px; height: 32px; border-radius: 16px" src="https://gravatar.com/avatar/f64fc44c03a8a7eb1d52502950879659?s=128" /></a>
           <a class="btn btn-sm btn-success mr-2" href="/create-post">Create Post</a>
-          <form action="/logout" method="POST" class="d-inline">
+          <form action="/logout" method="post" class="d-inline">
             @csrf
             <button class="btn btn-sm btn-secondary">Sign Out</button>
           </form>
         </div>
         @else
-        <form action="/login" method="POST" class="mb-0 pt-2 pt-md-0">
+        <form action="/login" method="post" class="mb-0 pt-2 pt-md-0">
           @csrf
           <div class="row align-items-center">
             <div class="col-md mr-0 pr-md-0 mb-3 mb-md-0">
