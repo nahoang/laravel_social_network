760b1445649e1a607deecfd5e6162e084b27a2ba
nahoang <nahoang9698@gmail.com>
2023-02-09 15:20:12 +0700

    feat: Checking admin permisson

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -26,6 +26,8 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        Gate::define('visitAdminPages', function($user) {
+            return $user->isAdmin === 1;
+        });
     }
 }
