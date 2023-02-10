45d09573f98b7640218862a9c276468643b616fe
nahoang <nahoang9698@gmail.com>
2023-02-10 22:13:27 +0700

    feat: Implement SPA for Tab component

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,11 @@
 import "./bootstrap";
 import Search from './live-search';
+import Profile from './profile';
+
+if (document.querySelector(".profile-nav")) {
+  new Profile();
+}
+
 
 if (document.querySelector(".header-search-icon")) {
   new Search();
