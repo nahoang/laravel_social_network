0670941a12291840423c1b0ce68b30a37175b45d
nahoang <nahoang9698@gmail.com>
2023-02-10 14:17:33 +0700

    feat: Implement get following and followed user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,14 +6,13 @@ use App\Models\User;
 use App\Models\Follow;
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rule;
+use Illuminate\Support\Facades\View;
 use Intervention\Image\Facades\Image;
 use Illuminate\Support\Facades\Storage;
 
 class UserController extends Controller
 {
-
-    public function storeAvatar(Request $request)
-    {
+    public function storeAvatar(Request $request) {
         $request->validate([
             'avatar' => 'required|image|max:3000'
         ]);
@@ -37,64 +36,73 @@ class UserController extends Controller
         return back()->with('success', 'Congrats on the new avatar.');
     }
 
-    public function showAvatarForm()
-    {
+    public function showAvatarForm() {
         return view('avatar-form');
     }
 
-    public function profile(User $user)
-    {
+    private function getSharedData($user) {
         $currentlyFollowing = 0;
 
         if (auth()->check()) {
             $currentlyFollowing = Follow::where([['user_id', '=', auth()->user()->id], ['followeduser', '=', $user->id]])->count();
         }
-        // return view('profile-posts', ['username' => $user->username, 'posts' => $user->posts()->latest()->get(), 'postCount' => $user->posts()->count()]);
-        return view('profile-posts', ['currentlyFollowing' => $currentlyFollowing, 'avatar' => $user->avatar, 'username' => $user->username, 'posts' => $user->posts()->latest()->get(), 'postCount' => $user->posts()->count()]);
-        
+
+        View::share('sharedData', ['currentlyFollowing' => $currentlyFollowing, 'avatar' => $user->avatar, 'username' => $user->username, 'postCount' => $user->posts()->count(), 'followerCount' => $user->followers()->count(), 'followingCount' => $user->followingTheseUsers()->count()]);
     }
 
-    public function showCorrectHomepage()
-    {
+    public function profile(User $user) {
+        $this->getSharedData($user);
+        return view('profile-posts', ['posts' => $user->posts()->latest()->get()]);
+    }
+
+    public function profileFollowers(User $user) {
+        $this->getSharedData($user);
+        return view('profile-followers', ['followers' => $user->followers()->latest()->get()]);
+    }
+
+    public function profileFollowing(User $user) {
+        $this->getSharedData($user);
+        return view('profile-following', ['following' => $user->followingTheseUsers()->latest()->get()]);
+    }
+
+    public function logout() {
+        auth()->logout();
+        return redirect('/')->with('success', 'You are now logged out.');
+    }
+
+    public function showCorrectHomepage() {
         if (auth()->check()) {
-            return view('homepage-feed');
+            return view('homepage-feed', ['posts' => auth()->user()->feedPosts()->latest()->paginate(4)]);
         } else {
             return view('homepage');
         }
     }
 
-    public function logout()
-    {
-        auth()->logout();
-        return redirect('/')->with('success', 'You are now logged out.');
-    }
-
-    public function login(Request $request)
-    {
+    public function login(Request $request) {
         $incomingFields = $request->validate([
             'loginusername' => 'required',
-            'loginpassword' => 'required',
+            'loginpassword' => 'required'
         ]);
 
         if (auth()->attempt(['username' => $incomingFields['loginusername'], 'password' => $incomingFields['loginpassword']])) {
             $request->session()->regenerate();
             return redirect('/')->with('success', 'You have successfully logged in.');
         } else {
-            return 'Sorry!!!';
+            return redirect('/')->with('failure', 'Invalid login.');
         }
     }
 
-    public function register(Request $request)
-    {
+    public function register(Request $request) {
         $incomingFields = $request->validate([
-            'username' => ['required', 'min:3', 'max: 20', Rule::unique('users', 'username')],
+            'username' => ['required', 'min:3', 'max:20', Rule::unique('users', 'username')],
             'email' => ['required', 'email', Rule::unique('users', 'email')],
-            'password' => ['required', 'min:8', 'confirmed'],
+            'password' => ['required', 'min:8', 'confirmed']
         ]);
 
         $incomingFields['password'] = bcrypt($incomingFields['password']);
 
-        User::create($incomingFields);
-        return 'Hello from register function';
+        $user = User::create($incomingFields);
+        auth()->login($user);
+        return redirect('/')->with('success', 'Thank you for creating an account.');
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -52,4 +52,17 @@ class User extends Authenticatable
     public function posts() {
         return $this->hasMany(Post::class, 'user_id');
     }
+
+    public function feedPosts() {
+        return $this->hasManyThrough(Post::class, Follow::class, 'user_id', 'user_id', 'id', 'followeduser');
+    }
+
+    public function followers() {
+        return $this->hasMany(Follow::class, 'followeduser');
+    }
+
+    public function followingTheseUsers() {
+        return $this->hasMany(Follow::class, 'user_id');
+    }
+
 }
